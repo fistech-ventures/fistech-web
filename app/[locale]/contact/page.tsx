fa7484b5addeaa36c7d52c85eba83b59a594885a
undefinedSahed <sahedrahmanshakil@gmail.com>
2026-03-29 11:16:33 +0600

    feat: added  page headline reusable component

---
+++ app/[locale]/contact/page.tsx
@@ -0,0 +1,10 @@
+import PageHeadline from "@/components/shared/page-headline";
+import React from "react";
+
+export default function ContactPage() {
+  return (
+    <div>
+      <PageHeadline headline="Contact Us" />
+    </div>
+  );
+}
