dca940fcfcea8309807da5414d95f2c153f6e381
undefinedSahed <sahedrahmanshakil@gmail.com>
2026-04-01 15:21:41 +0600

    refactor: solution card and solution details page started

--- app/[locale]/solutions/[slug]/page.tsx
+++ app/[locale]/solutions/[slug]/page.tsx
@@ -1,3 +1,6 @@
+import PageHeadline from "@/components/shared/page-headline";
+import SolutionHeading from "@/components/solution/solution-heading";
+import { SERVICES_DATA } from "@/data/service";
 import React from "react";
 
 export default async function SolutionDetails({
@@ -7,5 +10,12 @@ export default async function SolutionDetails({
 }) {
   const { slug } = await params;
 
-  return <div></div>;
+  const solution = SERVICES_DATA.find((item) => item.slug === slug);
+
+  return (
+    <>
+      <PageHeadline isSolution headline={solution?.meta.title || "Solution Details"} />
+      {/* <SolutionHeading headline={solution?.meta.title || "Solution Details"} /> */}
+    </>
+  );
 }
