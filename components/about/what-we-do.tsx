a0e1d6f71359184885f586ed6fae46bf5070a6f9
Fahim Sarker <sarkerfahim599@gmail.com>
2026-04-20 23:09:54 +0600

    fix: service section mobile view tags color fixed

--- components/about/what-we-do.tsx
+++ components/about/what-we-do.tsx
@@ -29,8 +29,8 @@ export default function Whatwedo() {
           scrollTrigger: {
             trigger: containerRef.current,
             start: "top 40%",
-            end: "bottom 90%",
-            scrub: 1.5,
+            end: "bottom 80%",
+            scrub: 2,
           },
         },
       );
@@ -42,15 +42,23 @@ export default function Whatwedo() {
     <section className="py-10" ref={containerRef}>
       <div className="container mx-auto relative">
         <SectionTag sectiontag="What We Do" />
-        <h2
-          className="section-title max-w-4xl mx-auto py-10 font-medium text-center"
-          style={{ fontFamily: "var(--font-kanit)" }}
-        >
-          {/* From strategy and design to content and technology — we scale your
-          vision into a market-leading reality. */}
+        <div className="relative">
+          <h2 className="section-title max-w-4xl mx-auto py-10 font-medium text-center text-black">
+            We help businesses turn ideas into real products that grow, reach
+            people, and perform in the real world.
+          </h2>
 
-          We help businesses turn ideas into real products that grow, reach people, and perform in the real world.
-        </h2>
+          <h2
+            ref={textRef}
+            className="section-title max-w-4xl mx-auto py-10 font-medium text-center text-gray-400 absolute inset-0"
+            style={{
+              clipPath: "inset(0 0% 0 0)",
+            }}
+          >
+            We help businesses turn ideas into real products that grow, reach
+            people, and perform in the real world.
+          </h2>
+        </div>
         <div className="absolute top-35 left-50">
           <Image
             width={150}
